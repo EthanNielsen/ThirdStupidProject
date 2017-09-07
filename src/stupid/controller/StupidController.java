d7ccfbb518aaf13d937f259818a7f155f3ab21c0
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-09-07 13:52:34 -0600

    I found out how to use a scanner object

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -1,10 +1,12 @@
 package stupid.controller;
 
+
+import java.util.Scanner;
+
 /**
- * 
  * @author enie1762 gethannielsengmail.com
- *
  */
+
 public class StupidController
 
 {
@@ -12,5 +14,18 @@ public class StupidController
 	public void start()
 	{
 		System.out.println("I am going to rock climbing today!");
+	
+		testScanner();
 	}
+	
+	private void testScanner()
+	{
+		Scanner firstScanner;
+		firstScanner = new Scanner(System.in);
+		System.out.println("What is your favorite food?");
+		String answer = firstScanner.nextLine();
+		System.out.println("Oh cool, you like " + answer);
+	}
+	
+
 }
